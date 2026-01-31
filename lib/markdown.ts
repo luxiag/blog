cfece38c1a5499875ae109bbc3449994de39bf79
luxiag <luxiag@qq.com>
2026-01-31 13:41:11 +0800

    fix:修复日志出错

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -141,7 +141,7 @@ export async function getPostData(slug: string): Promise<Post> {
           outputFormat: 'function-body',
           remarkPlugins: [remarkGfm, remarkMath, remarkDetails, [remarkAdmonitionsCustom, { keywords: ['details', 'note', 'warning', 'tip', 'important', 'info'] }]],
           rehypePlugins: [rehypeSlug, rehypeHighlight, rehypeKatex],
-          development: process.env.NODE_ENV === 'development',
+          development: false,
         });
         compiledContent = String(compiled);
         isMdxCompiled = true;
@@ -193,7 +193,7 @@ export async function getAllPosts(): Promise<Post[]> {
   const posts = await Promise.all(
     slugs.map(({ params }) => getPostData(params.slug))
   );
-  console.log('posts',posts)
+  console.log('posts', posts)
   return posts.sort((a, b) => {
     if (a.date < b.date) {
       return 1;
